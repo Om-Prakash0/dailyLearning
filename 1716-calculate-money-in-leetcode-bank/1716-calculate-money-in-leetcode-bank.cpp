7fb0a0152cce4e970a5413c9dc1432635a74390c
Om Prakash Yadav <omprakashyadavkone@gmail.com>
2026-06-30 10:45:30 +0530

    Time: 0 ms (100%), Space: 7.7 MB (100%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int amount = 0;
+
+        for(int i = 0; i < n; i++){
+            int week = i / 7;
+            int day = i % 7;
+
+            amount += week + day + 1;
+        }
+
+        return amount;
+    }
+};
