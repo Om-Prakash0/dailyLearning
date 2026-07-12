ae24ac577d346489cabd8a437ef3cf41a743f1e3
Om Prakash Yadav <omprakashyadavkone@gmail.com>
2026-07-12 12:58:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
         int n=arr.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i!=j && arr[i]==arr[j]*2){
-                    return true;
-                }
+        unordered_map<int,int>mp;
+        for(auto a: arr){
+            mp[a]++;
+        }
+        for (int i=0;i<n;i++){
+            if(mp[arr[i]*2]>0){
+                return true;
             }
         }
         return false;
