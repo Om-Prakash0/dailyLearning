219efae7aaccc323ce4501317946a81e53cec0bb
Om Prakash Yadav <omprakashyadavkone@gmail.com>
2026-09-02 22:58:43 +0530

    Time: 0 ms (100%), Space: 22.1 MB (6.59%) - LeetHub

--- 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.cpp
+++ 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.cpp
@@ -7,18 +7,17 @@ public:
         for(int i=0;i<n;i++){
             arr[nums[i]].push_back(i); 
         }
-        for(int i=0;i<=100;i++){
+        for(int i=0;i<arr.size();i++){
             if(arr[i].empty()){
                 continue;
             }
             int temp=arr[i][0];
             bool flag=true;
-            int k=0;
+        
             for(auto a: arr[i]){
                 if(temp!=a){
                     flag=false;
                 }
-                k++;
                 temp++;
             }
             if(flag) count++;
