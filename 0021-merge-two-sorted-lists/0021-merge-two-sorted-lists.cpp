260b86655702fab152f816939cf925bd47119eec
Om Prakash Yadav <omprakashyadavkone@gmail.com>
2026-09-13 22:11:16 +0530

    Time: 0 ms (100%), Space: 19.9 MB (8.24%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -20,13 +20,12 @@ public:
 
         if (arr.empty()) return nullptr;
 
-        ListNode* head = new ListNode(arr[0]);
-        ListNode* curr = head;
-
-        for (int i = 1; i < arr.size(); i++) {
-            curr->next = new ListNode(arr[i]);
-            curr = curr->next;
-        }
+       ListNode* head=new ListNode(arr[0]);
+       ListNode* curr=head;
+       for(int i=1;i<arr.size();i++){
+         curr->next=new ListNode(arr[i]);
+         curr=curr->next;
+       }
 
         return head;
     }
