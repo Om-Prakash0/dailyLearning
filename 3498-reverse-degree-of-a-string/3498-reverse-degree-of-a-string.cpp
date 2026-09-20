6da54f597672313a4917e4b37ae2b9b8171ffdd2
Om Prakash Yadav <omprakashyadavkone@gmail.com>
2026-09-20 10:21:53 +0530

    Time: 3 ms (16.14%), Space: 9.8 MB (47.88%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,15 +1,29 @@
+// class Solution {
+// public:
+//     int reverseDegree(string s) {
+//        vector<char>c={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+//        unordered_map<char,int>mp;
+//        for(int i=0;i<26;i++){
+//             mp[c[i]]=26-i;
+//        }
+//        int sum=0;
+//        for(int i=0;i<s.size();i++){
+//         sum+=mp[s[i]]*(i+1);
+//        }
+//        return sum;
+//     }
+// };
+
 class Solution {
 public:
     int reverseDegree(string s) {
-       vector<char>c={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-       unordered_map<char,int>mp;
-       for(int i=0;i<26;i++){
-            mp[c[i]]=26-i;
-       }
-       int sum=0;
-       for(int i=0;i<s.size();i++){
-        sum+=mp[s[i]]*(i+1);
-       }
-       return sum;
+        int sum = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            int value = 'z' - s[i] + 1;
+            sum += value * (i + 1);
+        }
+
+        return sum;
     }
 };
